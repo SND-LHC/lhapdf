1d70ebd1b3e2aa4778f05dc706d029491ade9c3c
Andy Buckley <andy.buckley@cern.ch>
2020-05-30 21:52:41 +0100

    Fix cache logic... but I don't like the look of the hash values

--- src/LogBicubicInterpolator.cc
+++ src/LogBicubicInterpolator.cc
@@ -70,7 +70,8 @@ namespace LHAPDF {
     // Update the x cache
     const size_t xhash = subgrid.xhash();
     const bool xok = instance.x == x;
-    const bool ixok = false; //instance.ix != ix || instance.xhash != xhash;
+    cout << instance.xhash << " " << xhash << endl;
+    const bool ixok = instance.ix == ix && instance.xhash == xhash;
     if (!xok) {
       instance.logx = log(x);
     }
@@ -85,7 +86,7 @@ namespace LHAPDF {
     // Update the Q2 cache
     const size_t q2hash = subgrid.q2hash();
     const bool q2ok = instance.q2 == q2;
-    const bool iq2ok = false; //instance.iq2 != iq2 || instance.q2hash != q2hash;
+    const bool iq2ok = instance.iq2 == iq2 && instance.q2hash == q2hash;
     if (!q2ok) {
       instance.logq2 = log(q2);
     }
