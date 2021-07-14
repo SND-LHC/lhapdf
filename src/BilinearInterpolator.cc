c165b928d930058a621b1e9ab05fe16140734202
max.knobbe <max.knobbe@stud.uni-goettingen.de>
2021-07-14 21:52:05 +0200

    translate bilinearinterpolator to new data structure

--- src/BilinearInterpolator.cc
+++ src/BilinearInterpolator.cc
@@ -11,7 +11,7 @@ namespace LHAPDF {
   namespace { // Unnamed namespace
 
     // One-dimensional linear interpolation for y(x)
-    inline double _interpolateLinear(double x, double xl, double xh, double yl, double yh)	{
+    inline double _interpolateLinear(const double x, const double xl, const double xh, const double yl, const double yh){
       assert(x >= xl);
       assert(xh >= x);
       return yl + (x - xl) / (xh - xl) * (yh - yl);
@@ -20,16 +20,19 @@ namespace LHAPDF {
   }
 
 
-  double BilinearInterpolator::_interpolateXQ2(const KnotArray1F& subgrid, double x, size_t ix, double q2, size_t iq2) const {
+  double BilinearInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, int id) const {
+    /*
     if (subgrid.logxs().size() < 2)
       throw GridError("PDF subgrids are required to have at least 2 x-knots for use with BilinearInterpolator");
     if (subgrid.logq2s().size() < 2)
       throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BilinearInterpolator");
+    */
+    
     // First interpolate in x
-    const double f_ql = _interpolateLinear(x, subgrid.xs()[ix], subgrid.xs()[ix+1], subgrid.xf(ix, iq2), subgrid.xf(ix+1, iq2));
-    const double f_qh = _interpolateLinear(x, subgrid.xs()[ix], subgrid.xs()[ix+1], subgrid.xf(ix, iq2+1), subgrid.xf(ix+1, iq2+1));
+    const double f_ql = _interpolateLinear(x, grid.xs(ix), grid.xs(ix+1), grid.xf(ix, iq2, id), grid.xf(ix+1, iq2, id));
+    const double f_qh = _interpolateLinear(x, grid.xs(ix), grid.xs(ix+1), grid.xf(ix, iq2+1, id), grid.xf(ix+1, iq2+1, id));
     // Then interpolate in Q2, using the x-ipol results as anchor points
-    return _interpolateLinear(q2, subgrid.q2s()[iq2], subgrid.q2s()[iq2+1], f_ql, f_qh);
+    return _interpolateLinear(q2, grid.q2s(iq2), grid.q2s(iq2+1), f_ql, f_qh);
   }
 
 
