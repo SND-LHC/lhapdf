70e70f5822abc6ec42f5f5a393e3b34bf188726c
max.knobbe <max.knobbe@stud.uni-goettingen.de>
2021-07-28 17:33:05 +0200

    translate bicubicinterpolator
    currently only works for continuous sets

--- src/BicubicInterpolator.cc
+++ src/BicubicInterpolator.cc
@@ -18,7 +18,7 @@ namespace LHAPDF {
       return yl + (x - xl) / (xh - xl) * (yh - yl);
     }
 
-    // One-dimensional cubic interpolation
+    // One-dimensional cubic interpolation    
     inline double _interpolateCubic(double T, double VL, double VDL, double VH, double VDH) {
       // Pre-calculate powers of T
       const double t2 = T*T;
@@ -37,15 +37,15 @@ namespace LHAPDF {
 
 
     // Provides d/dx at all grid locations
-    double _ddx(const KnotArray1F& subgrid, size_t ix, size_t iq2) {
+    double _ddx(const KnotArray& grid, size_t ix, size_t iq2, int id) {
       /// @todo Re-order this if so that branch prediction will favour the "normal" central case
       if (ix == 0) { //< If at leftmost edge, use forward difference
-        return (subgrid.xf(ix+1, iq2) - subgrid.xf(ix, iq2)) / (subgrid.xs()[ix+1] - subgrid.xs()[ix]);
-      } else if (ix == subgrid.xs().size() - 1) { //< If at rightmost edge, use backward difference
-        return (subgrid.xf(ix, iq2) - subgrid.xf(ix-1, iq2)) / (subgrid.xs()[ix] - subgrid.xs()[ix-1]);
+        return (grid.xf(ix+1, iq2, id) - grid.xf(ix, iq2, id)) / (grid.xs(ix+1) - grid.xs(ix));
+      } else if (ix == grid.xsize() - 1) { //< If at rightmost edge, use backward difference
+        return (grid.xf(ix, iq2, id) - grid.xf(ix-1, iq2, id)) / (grid.xs(ix) - grid.xs(ix-1));
       } else { //< If central, use the central difference
-        const double lddx = (subgrid.xf(ix, iq2) - subgrid.xf(ix-1, iq2)) / (subgrid.xs()[ix] - subgrid.xs()[ix-1]);
-        const double rddx = (subgrid.xf(ix+1, iq2) - subgrid.xf(ix, iq2)) / (subgrid.xs()[ix+1] - subgrid.xs()[ix]);
+        const double lddx = (grid.xf(ix, iq2, id) - grid.xf(ix-1, iq2, id)) / (grid.xs(ix) - grid.xs(ix-1));
+        const double rddx = (grid.xf(ix+1, iq2, id) - grid.xf(ix, iq2, id)) / (grid.xs(ix+1) - grid.xs(ix));
         return (lddx + rddx) / 2.0;
       }
     }
@@ -53,40 +53,40 @@ namespace LHAPDF {
   }
 
 
+  
+  double BicubicInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, int id) const {
 
-  double BicubicInterpolator::_interpolateXQ2(const KnotArray1F& subgrid, double x, size_t ix, double q2, size_t iq2) const {
-    if (subgrid.logxs().size() < 4)
-      throw GridError("PDF subgrids are required to have at least 4 x-knots for use with BicubicInterpolator");
-    if (subgrid.logq2s().size() < 4) {
-      if (subgrid.logq2s().size() > 1) {
-	// Fallback to BilinearInterpolator if either 2 or 3 Q2-knots
-	// First interpolate in x
-	const double f_ql = _interpolateLinear(x, subgrid.xs()[ix], subgrid.xs()[ix+1], subgrid.xf(ix, iq2), subgrid.xf(ix+1, iq2));
-	const double f_qh = _interpolateLinear(x, subgrid.xs()[ix], subgrid.xs()[ix+1], subgrid.xf(ix, iq2+1), subgrid.xf(ix+1, iq2+1));
-	// Then interpolate in Q2, using the x-ipol results as anchor points
-	return _interpolateLinear(q2, subgrid.q2s()[iq2], subgrid.q2s()[iq2+1], f_ql, f_qh);
-      } else throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BicubicInterpolator");
-    }
-
+    // MK: if they have at least 2 knots, falls back to linear interpolator
+    if (grid.xsize() < 4)
+      throw GridError("PDF subgrids are required to have at least 4 x-knots for use with BicubicInterpolator");    
+    if (grid.q2size() < 4) 
+      throw GridError("PDF subgrids are required to have at least 4 Q2-knots for use with BicubicInterpolator");
+    
     /// @todo Allow interpolation right up to the borders of the grid in Q2 and x... the last inter-knot range is currently broken
-
     /// @todo Also treat the x top/bottom edges carefully, cf. the Q2 ones
 
+    // check edges, including internal discontinuity
+    const bool q2_lower = ( (iq2 == 0) && (grid.q2s(iq2) == grid.q2s(iq2-1)));
+    const bool q2_upper = ( (iq2 == grid.q2size() -1) && (grid.q2s(iq2+1) == grid.q2s(iq2+2)) );
+    const bool ix_lower = ( (ix == 0) );
+    const bool ix_upper = ( (ix == grid.xsize()) );
+    
     // Distance parameters
-    const double dx = subgrid.xs()[ix+1] - subgrid.xs()[ix];
-    const double tx = (x - subgrid.xs()[ix]) / dx;
+    const double dx = grid.xs(ix+1) - grid.xs(ix);
+    const double tx = (x - grid.xs(ix)) / dx;
     /// @todo Only compute these if the +1 and +2 indices are guaranteed to be valid
-    const double dq_0 = subgrid.q2s()[iq2] - subgrid.q2s()[iq2-1];
-    const double dq_1 = subgrid.q2s()[iq2+1] - subgrid.q2s()[iq2];
-    const double dq_2 = subgrid.q2s()[iq2+2] - subgrid.q2s()[iq2+1];
+    // i.e. check if that is in range, and there is no discontinuitie there
+    const double dq_0 = grid.q2s(iq2) - grid.q2s(iq2-1);
+    const double dq_1 = grid.q2s(iq2+1) - grid.q2s(iq2);
+    const double dq_2 = grid.q2s(iq2+2) - grid.q2s(iq2+1);
     const double dq = dq_1;
-    const double tq = (q2 - subgrid.q2s()[iq2]) / dq;
+    const double tq = (q2 - grid.q2s(iq2)) / dq;
 
     // Points in Q2
-    double vl = _interpolateCubic(tx, subgrid.xf(ix, iq2), _ddx(subgrid, ix, iq2) * dx,
-                                      subgrid.xf(ix+1, iq2), _ddx(subgrid, ix+1, iq2) * dx);
-    double vh = _interpolateCubic(tx, subgrid.xf(ix, iq2+1), _ddx(subgrid, ix, iq2+1) * dx,
-                                      subgrid.xf(ix+1, iq2+1), _ddx(subgrid, ix+1, iq2+1) * dx);
+    double vl = _interpolateCubic(tx, grid.xf(ix, iq2, id), _ddx(grid, ix, iq2, id) * dx,
+				  grid.xf(ix+1, iq2, id), _ddx(grid, ix+1, iq2, id) * dx);
+    double vh = _interpolateCubic(tx, grid.xf(ix, iq2+1, id), _ddx(grid, ix, iq2+1, id) * dx,
+				  grid.xf(ix+1, iq2+1, id), _ddx(grid, ix+1, iq2+1, id) * dx);
 
     // Derivatives in Q2
     double vdl, vdh;
@@ -94,25 +94,25 @@ namespace LHAPDF {
       // Forward difference for lower q
       vdl = (vh - vl) / dq_1;
       // Central difference for higher q
-      double vhh = _interpolateCubic(tx, subgrid.xf(ix, iq2+2), _ddx(subgrid, ix, iq2+2) * dx,
-                                         subgrid.xf(ix+1, iq2+2), _ddx(subgrid, ix+1, iq2+2) * dx);
+      double vhh = _interpolateCubic(tx, grid.xf(ix, iq2+2, id), _ddx(grid, ix, iq2+2, id) * dx,
+				     grid.xf(ix+1, iq2+2, id), _ddx(grid, ix+1, iq2+2, id) * dx);
       vdh = (vdl + (vhh - vh)/dq_2) / 2.0;
     }
-    else if (iq2+1 == subgrid.q2s().size()-1) {
+    else if (iq2+1 == grid.q2size()-1) {
       // Backward difference for higher q
       vdh = (vh - vl) / dq_1;
       // Central difference for lower q
-      double vll = _interpolateCubic(tx, subgrid.xf(ix, iq2-1), _ddx(subgrid, ix, iq2-1) * dx,
-                                         subgrid.xf(ix+1, iq2-1), _ddx(subgrid, ix+1, iq2-1) * dx);
+      double vll = _interpolateCubic(tx, grid.xf(ix, iq2-1, id), _ddx(grid, ix, iq2-1, id) * dx,
+				     grid.xf(ix+1, iq2-1, id), _ddx(grid, ix+1, iq2-1, id) * dx);
       vdl = (vdh + (vl - vll)/dq_0) / 2.0;
     }
     else {
       // Central difference for both q
-      double vll = _interpolateCubic(tx, subgrid.xf(ix, iq2-1), _ddx(subgrid, ix, iq2-1) * dx,
-                                         subgrid.xf(ix+1, iq2-1), _ddx(subgrid, ix+1, iq2-1) * dx);
+      double vll = _interpolateCubic(tx, grid.xf(ix, iq2-1, id), _ddx(grid, ix, iq2-1, id) * dx,
+				     grid.xf(ix+1, iq2-1, id), _ddx(grid, ix+1, iq2-1, id) * dx);
       vdl = ( (vh - vl)/dq_1 + (vl - vll)/dq_0 ) / 2.0;
-      double vhh = _interpolateCubic(tx, subgrid.xf(ix, iq2+2), _ddx(subgrid, ix, iq2+2) * dx,
-                                         subgrid.xf(ix+1, iq2+2), _ddx(subgrid, ix+1, iq2+2) * dx);
+      double vhh = _interpolateCubic(tx, grid.xf(ix, iq2+2, id), _ddx(grid, ix, iq2+2, id) * dx,
+				     grid.xf(ix+1, iq2+2, id), _ddx(grid, ix+1, iq2+2, id) * dx);
       vdh = ( (vh - vl)/dq_1 + (vhh - vh)/dq_2 ) / 2.0;
     }
 
