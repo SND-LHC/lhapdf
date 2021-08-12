f8aceae3d036050b0f6e243436590dcf0a8975d6
max.knobbe <max.knobbe@stud.uni-goettingen.de>
2021-08-12 21:16:25 +0200

    refactor common code in the linear interpolators

--- src/BilinearInterpolator.cc
+++ src/BilinearInterpolator.cc
@@ -25,31 +25,32 @@ namespace LHAPDF {
       // Then interpolate in Q2, using the x-ipol results as anchor points
       return _interpolateLinear(q2, grid.q2s(iq2), grid.q2s(iq2+1), f_ql, f_qh);
     }
+    
+    void _checkGridSize(const KnotArray &grid){
+      if (grid.xsize() < 2)
+	throw GridError("PDF subgrids are required to have at least 2 x-knots for use with BilinearInterpolator");
+      if (grid.q2size() < 2)
+	throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BilinearInterpolator");
+    }
+
   }
   
 
   double BilinearInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, int id) const {
-    if (grid.xsize() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 x-knots for use with BilinearInterpolator");
-    if (grid.q2size() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BilinearInterpolator");
-
+    _checkGridSize(grid);
     return _interpolateSinglePid(grid, x, ix, q2, iq2, id);
   }
 
 
   void BilinearInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, std::vector<double>& ret) const{
-    if (grid.xsize() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 x-knots for use with BilinearInterpolator");
-    if (grid.q2size() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BilinearInterpolator");
-    ret.clear();    
+    _checkGridSize(grid);
+    ret.resize(13);    
     for(int pid(-6); pid <= 6; ++pid){
       int id = grid._lookup[pid + 6];
       if(id == -1){
-	ret.push_back(0);
+	ret[pid + 6] = 0;
       } else {
-	ret.push_back(_interpolateSinglePid(grid, x, ix, q2, iq2, id));
+	ret[pid + 6] = _interpolateSinglePid(grid, x, ix, q2, iq2, id);
       }
     }
   }
--- src/LogBilinearInterpolator.cc
+++ src/LogBilinearInterpolator.cc
@@ -9,7 +9,25 @@ namespace LHAPDF {
 
 
   namespace { // Unnamed namespace
+    struct shared_data{
+      // manual cache some values
+      double logx;
+      double logq2;
+      double logx0;
+      double logx1;
+    };
 
+    shared_data fill(const KnotArray& grid, double x, double q2, size_t ix){
+      // manual cache some values
+      shared_data share;
+      share.logq2 = log(q2);
+      share.logx  = log(x);
+      share.logx0 = grid.logxs(ix);
+      share.logx1 = grid.logxs(ix+1);
+      
+      return share;
+    }
+    
     // One-dimensional linear interpolation for y(x)
     inline double _interpolateLinear(double x, double xl, double xh, double yl, double yh)	{
       assert(x >= xl);
@@ -17,51 +35,42 @@ namespace LHAPDF {
       return yl + (x - xl) / (xh - xl) * (yh - yl);
     }
 
+    void _checkGridSize(const KnotArray &grid){
+      if (grid.xsize() < 2)
+	throw GridError("PDF subgrids are required to have at least 2 x-knots for use with BilinearInterpolator");
+      if (grid.q2size() < 2)
+	throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with BilinearInterpolator");
+    }
+
+    double _interpolate(const KnotArray& grid, size_t ix, size_t iq2, int id, shared_data _share){
+      const double f_ql = _interpolateLinear(_share.logx, _share.logx0, _share.logx1, grid.xf(ix, iq2, id), grid.xf(ix+1, iq2, id));
+      const double f_qh = _interpolateLinear(_share.logx, _share.logx0, _share.logx1, grid.xf(ix, iq2+1, id), grid.xf(ix+1, iq2+1, id));
+      // Then interpolate in Q2, using the x-ipol results as anchor points
+      return _interpolateLinear(_share.logq2, grid.logq2s(iq2), grid.logq2s(iq2+1), f_ql, f_qh);
+    }
   }
   
   void LogBilinearInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, std::vector<double>& ret) const{
-    if (grid.xsize() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 x-knots for use with LogBilinearInterpolator");
-    if (grid.q2size() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with LogBilinearInterpolator");
-
-    // manual cache some values
-    const double logx = log(x);
-    const double logx0 = grid.logxs(ix);
-    const double logx1 = grid.logxs(ix+1);
+    _checkGridSize(grid);
+    shared_data shared = fill(grid, x, q2, ix);
     
     // First interpolate in x
     ret.clear();
     for(int pid(-6); pid <= 6; ++pid){
       int id = grid._lookup[pid + 6];
       if(id == -1){
-	ret.push_back(0);
+	ret[pid + 6] = 0;
       } else {
-	const double f_ql = _interpolateLinear(logx, logx0, logx1, grid.xf(ix, iq2, id), grid.xf(ix+1, iq2, id));
-	const double f_qh = _interpolateLinear(logx, logx0, logx1, grid.xf(ix, iq2+1, id), grid.xf(ix+1, iq2+1, id));
-	// Then interpolate in Q2, using the x-ipol results as anchor points
-	ret.push_back(_interpolateLinear(log(q2), grid.logq2s(iq2), grid.logq2s(iq2+1), f_ql, f_qh));
+	ret[pid + 6] = _interpolate(grid, ix, iq2, id, shared);
       }
     }
   }
 
   
   double LogBilinearInterpolator::_interpolateXQ2(const KnotArray& grid, double x, size_t ix, double q2, size_t iq2, int id) const {
-    if (grid.xsize() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 x-knots for use with LogBilinearInterpolator");
-    if (grid.q2size() < 2)
-      throw GridError("PDF subgrids are required to have at least 2 Q2-knots for use with LogBilinearInterpolator");
-
-    // manual cache some values
-    const double logx = log(x);
-    const double logx0 = grid.logxs(ix);
-    const double logx1 = grid.logxs(ix+1);
-    
-    // First interpolate in x
-    const double f_ql = _interpolateLinear(logx, logx0, logx1, grid.xf(ix, iq2, id), grid.xf(ix+1, iq2, id));
-    const double f_qh = _interpolateLinear(logx, logx0, logx1, grid.xf(ix, iq2+1, id), grid.xf(ix+1, iq2+1, id));
-    // Then interpolate in Q2, using the x-ipol results as anchor points
-    return _interpolateLinear(log(q2), grid.logq2s(iq2), grid.logq2s(iq2+1), f_ql, f_qh);
+    _checkGridSize(grid);
+    shared_data shared = fill(grid, x, q2, ix);
+    return _interpolate(grid, ix, iq2, id, shared);
   }
 
 
