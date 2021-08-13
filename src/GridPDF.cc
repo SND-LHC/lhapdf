9bf6453f838c9e2f9217e75ad8a004a09c689dac
Andy Buckley <andy.buckley@cern.ch>
2021-08-13 14:05:18 +0100

    Update the fast ASCII NumParser to always use the C locale. Thanks to Dmitry Kalinkin for highlighting the issue.

--- src/GridPDF.cc
+++ src/GridPDF.cc
@@ -9,6 +9,7 @@
 #include "LHAPDF/FileIO.h"
 #include <iostream>
 #include <sstream>
+#include <locale>
 #include <string>
 #include <stdexcept>
 #include <cstring>
@@ -119,9 +120,19 @@ namespace LHAPDF {
     class NumParser {
     public:
       // Constructor from char*
-      NumParser(const char* line=0) { reset(line); }
+      NumParser(const char* line=0) {
+        reset(line);
+        _set_locale();
+      }
       // Constructor from std::string
-      NumParser(const string& line) { reset(line); }
+      NumParser(const string& line) {
+        reset(line);
+        _set_locale();
+      }
+      // Destructor
+      ~NumParser() {
+        _reset_locale();
+      }
 
       // Re-init to new line as char*
       void reset(const char* line=0) {
@@ -143,13 +154,33 @@ namespace LHAPDF {
       // Allow use of operator>> in a while loop
       operator bool() const { return !_error; }
 
+
     private:
+
+      // Changes the thread-local locale to interpret numbers in the "C" locale
+      void _set_locale() {
+        _locale_set = newlocale(LC_NUMERIC_MASK, "C", NULL);
+        _locale_prev = uselocale(_locale_set);
+        if (!_locale_prev) {
+          throw ReadError(std::string("Error setting locale: ") + strerror(errno));
+        }
+      }
+      // Returns the locale to the original setting
+      void _reset_locale() {
+        if (!uselocale(_locale_prev)) {
+          throw ReadError(std::string("Error setting locale: ") + strerror(errno));
+        }
+        freelocale(_locale_set);
+      }
+
       void _get(double& x) { x = std::strtod(_next, &_new_next); }
       void _get(float& x) { x = std::strtof(_next, &_new_next); }
       void _get(int& i) { i = std::strtol(_next, &_new_next, 10); } // force base 10!
 
+      locale_t _locale_set, _locale_prev;
       char *_next, *_new_next;
       bool _error;
+
     };
 
   }
